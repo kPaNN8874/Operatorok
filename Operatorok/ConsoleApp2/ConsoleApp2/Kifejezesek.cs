3b605b91a64581d424d66df2a78f9a920a418506
kPaNN8874 <113461676+kPaNN8874@users.noreply.github.com>
2023-05-24 11:58:40 +0200

    Add files via upload

--- Operatorok/ConsoleApp2/ConsoleApp2/Kifejezesek.cs
+++ Operatorok/ConsoleApp2/ConsoleApp2/Kifejezesek.cs
@@ -8,6 +8,27 @@ namespace ConsoleApp2
 {
     internal class Kifejezesek
     {
+        int elsoOperandus;
+        string oper;
+        int masodikOperandus;
 
+         public Kifejezesek(string csvSor)
+        {
+            var mezok = csvSor.Split(';');
+            this.elsoOperandus = mezok[0];
+            this.oper = mezok[1];
+            this.masodikOperandus = mezok[2];
+        }
+         public Kolcsonzes(string nev, char jazon, int eOra, int ePerc, int vOra, int vPerc)
+        {
+            this.elsoOperandus = elsoOperandus;
+            this.oper = oper;
+            this.masodikOperandus=masodikOperandus;
+        }
+        
+        public int elsoOperandus { get => elsoOperandus; }
+        public string oper { get => oper; }
+        public int masodikOperandus { get => masodikOperandus; }
+          
     }
 }
--- Operatorok/ConsoleApp2/ConsoleApp2/Program.cs
+++ Operatorok/ConsoleApp2/ConsoleApp2/Program.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ConsoleApp2;
+using System.Linq;
+
+internal class Program { 
+    static List<Kifejezesek> kifejezesek = new List<Kifejezesek>();
+            static void Main(string[] args) { 
+         foreach (var sor in File.ReadAllLines("DataSource\\kifejezesek.csv"))
+            {
+                kifejezesek.Add(new Kifejezesek(sor));
+            }
+         //6.feladat
+           public string Ertek()
+        {
+            var ertek = string.Empty;
+            try
+            {
+                switch (this.kifejezesek)
+                {
+                    case "mod":
+                        ertek = $"{this.elsoOperandus % this.masodikOperandus}";
+                        break;
+                    case "div":
+                        ertek = $"{this.elsoOperandus / this.masodikOperandus}";
+                        break;
+                    case "+":
+                        ertek = $"{this.elsoOperandus + this.masodikOperandus}";
+                        break;
+                    case "-":
+                        ertek = $"{this.elsoOperandus - this.masodikOperandus}";
+                        break;
+                    case "*":
+                        ertek = $"{this.elsoOperandus * this.masodikOperandus}";
+                        break;
+                    case "/":
+                        ertek = $"{(double)this.elsoOperandus / this.masodikOperandus}";
+                        break;
+                    default:
+                        ertek = "Hibás operátor!";
+                        break;
+                }
+            }
+            catch (Exception)
+            {
+                ertek = "Egyéb hiba!";
+            }
+
+            return ertek;
+        }
+        public override string ToString()
+        {
+            return $"{this.elsoOperandus} {this.kifejezesek} {this.masodikOperandus} = {this.Ertek()}";
+        }
+    }
+         //2.feladta
+         Console.WriteLine($"2.feladat: Kifejezések száma: {kifejezesek.Count}");
+         //3.feladat
+         Console.WriteLine($"3.feladat: Kifejezések maradékos osztással: {kifejezesek.Where(x=>x.oper == "mod").Count()}");
+        //4.feladat
+        Console.WriteLine($"4.feladat: {kifejezesek.Where(x=>x.elsoOperandus%10 == 0 && x.masodikOperandus%10 == 0)}");
+        //5.feladat
+        Console.WriteLine($"5.feladat: Statisztika \n mod -> {kifejezesek.Where(x=>x.oper == "mod").Count()} \n / -> { kifejezesek.Where(x=>x.oper == "/").Count()} \n div ->{kifejezesek.Where(x=>x.oper == "div").Count()} \n - -> {kifejezesek.Where(x=>x.oper== "-").Count()} \n * -> {kifejezesek.Where(x=>x.oper == "*").Count()} \n + -> {kifejezesek.Where(x=>x.oper=="+").Count()}");
+        //7.feladat
+       do 
+            {
+                Console.Write($"7. feladat: Kérek egy kifejezést (pl.: 1 + 1): ");
+                string input = Console.ReadLine();
+
+                if (input == "vége")
+                {
+                    break;
+                }
+
+                Console.WriteLine($"{input} = {new Operandus(input).Ertek()}");
+            }
+            while (true);
+        //8.feladat
+        StreamWriter streamWriter = new StreamWriter("eredmenyek.csv");
+            foreach (var item in operandusok)
+            {
+                streamWriter.WriteLine(item.ToString());
+            }
+        streamWriter.Flush();
+        streamWriter.Close();
+    }
+ }
